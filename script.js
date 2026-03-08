a2262b94375750a82328197c04d11275114701cd
tiagohsa <tiago.alieve@gmail.com>
2026-03-08 16:13:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+async function pagar(valor,idNome,idWhatsapp){
+  const nome = document.getElementById(idNome).value
+  const whatsapp = document.getElementById(idWhatsapp).value
+
+  if(!nome || !whatsapp){
+    alert("Preencha nome e WhatsApp")
+    return
+  }
+
+  try{
+    const res = await fetch("http://localhost:3000/criar-pix",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body: JSON.stringify({nome,whatsapp,valor})
+    })
+
+    const dados = await res.json()
+    window.location.href = dados.urlPix
+
+  }catch(e){
+    alert("Erro ao criar pagamento Pix. Backend não está rodando.")
+    console.log(e)
+  }
+}
